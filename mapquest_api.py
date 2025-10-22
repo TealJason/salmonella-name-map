9db5862d81197f68ccbf781679e5af1033fa16af
Jason Beard <jason.beard@phe.gov.uk@phe-pf12qyrt.eud.unix.phe.gov.uk>
2025-10-22 16:58:04 +0100

    sys exit on no api key, should change this to fail more gracefully

--- mapquest_api.py
+++ mapquest_api.py
@@ -9,6 +9,7 @@ def make_mapbox_image(coordinate):
     except IOError as e:
         print(f"couldn't get the api key {e}")
         print("get a valid key or use coordinates as an input")
+        sys.exit(1)
         
     lat, long = coordinate
     subprocess.run(f"curl -g 'https://api.mapbox.com/styles/v1/mapbox/streets-v12/static/{long},{lat},10,0,0/600x600?access_token={token}&attribution=false'  --output serovar-location.png", shell=True)
