7191efff10e5e1fc00724db543e46c63ed6c60f0
Jason Beard <jason.beard@phe.gov.uk@phe-pf12qyrt.eud.unix.phe.gov.uk>
2025-10-23 15:55:49 +0100

    clean up way to get formula for salmonella serovars

--- get_antigentic_formula.py
+++ get_antigentic_formula.py
@@ -15,29 +15,29 @@ def argument_passing():
     args = parser.parse_args()
     return args
 
-def get_antigens_for_serovar(closest_name):
+def get_antigens_for_serovar(name):
     try:
-        with open("./get_distance/serovar_name_antigen.json", "r") as f:
+        with open("full_name_to_serovar.json", "r") as f:
             serovar_antigen_dict = json.load(f)
     except IOError:
         print("Couldn't open the name:antigen JSON file.")
         sys.exit(1)
     
-    if closest_name not in serovar_antigen_dict:
-        print(f"Serovar '{closest_name}' not found in the JSON file.")
+    if name not in serovar_antigen_dict:
+        print(f"Serovar '{name}' not found in the JSON file.")
         sys.exit(1)
 
-    filtered_dict = serovar_antigen_dict[closest_name]
+    filtered_dict = serovar_antigen_dict[name]
 
     h_antigen = filtered_dict.get("H-Antigen", "unknown")
     o_antigen_p1 = filtered_dict.get("O-AntigenP1", "unknown")
     o_antigen_p2 = filtered_dict.get("O-AntigenP2", "unknown")   
     
-    print(f"The antigenic formula is {h_antigen}:{o_antigen_p1}:{o_antigen_p2}")
+    print(f"The antigenic formula for salmonella {name} is {h_antigen}:{o_antigen_p1}:{o_antigen_p2}")
 
 def main():
     args = argument_passing()
-    get_antigens_for_serovar(args.name)
+    get_antigens_for_serovar(args.name.lower())
 
 if __name__ == "__main__":
     main()
