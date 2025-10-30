bc3be109be6733821a1c7047deaa33acd1773f93
TealJason <jasonthomasbeard@gmail.com>
2025-10-30 18:33:18 +0000

    fix the antigen finder

--- website/salmon_site/antigen_finder/utils.py
+++ website/salmon_site/antigen_finder/utils.py
@@ -2,15 +2,18 @@
 import sys
 import json
 import argparse 
+import os 
 
 def get_antigens_for_serovar(name):  
+
+    coordinate_path = os.path.join(os.path.dirname(__file__), "data", "serovar_name_antigen.json")
+    
     try:
-        with open("/home/phe.gov.uk/jason.beard/Desktop/projects/salmon_map/project/salmonella-name-map/website/salmon_site/antigen_finder/data/serovar_name_antigen.json", "r") as f:
+        with open(coordinate_path, "r") as f:
             serovar_antigen_dict = json.load(f)
     except IOError:
-        print("Couldn't open the JSON file.")
-        sys.exit(1)
-    
+        raise FileNotFoundError("Couldn't open the name-to-antigen JSON.")
+
     if name not in serovar_antigen_dict:
         print(f"Serovar '{name}' not found in the JSON file.")
         sys.exit(1)
@@ -21,4 +24,11 @@ def get_antigens_for_serovar(name):
     o_antigen_p1 = filtered_dict.get("O-AntigenP1", "unknown")
     o_antigen_p2 = filtered_dict.get("O-AntigenP2", "unknown")   
     
-    return name, h_antigen, o_antigen_p1, o_antigen_p2
+    result_dict = {
+        "h_antigen":h_antigen,
+        "o_AntigenPhase1":o_antigen_p1,
+        "o_AntigenPhase2":o_antigen_p2,
+        "serovar_name":name
+    }
+
+    return result_dict
