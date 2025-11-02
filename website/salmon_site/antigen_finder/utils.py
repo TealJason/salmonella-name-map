4911481ff2d7c4c5f4ce8cdf695200e86a87ea5c
TealJason <jasonthomasbeard@gmail.com>
2025-11-02 20:55:59 +0000

    fix json h-o antigen mix up + paratyphi fun

--- website/salmon_site/antigen_finder/utils.py
+++ website/salmon_site/antigen_finder/utils.py
@@ -8,7 +8,8 @@ def get_antigens_for_serovar(name):
 
     coordinate_path = os.path.join(os.path.dirname(__file__), "data", "serovar_name_antigen.json")
     
-    name = name.capitalize()
+    
+    search_name = name.capitalize()
 
     try:
         with open(coordinate_path, "r") as f:
@@ -16,27 +17,27 @@ def get_antigens_for_serovar(name):
     except IOError:
         raise FileNotFoundError("Couldn't open the name-to-antigen JSON.")
 
-    if name not in serovar_antigen_dict:
+    if search_name not in serovar_antigen_dict:
         print(f"Serovar '{name}' not found in the JSON file.")
         empty = {}
         return empty 
 
     try:
-        filtered_dict = serovar_antigen_dict[name]
+        filtered_dict = serovar_antigen_dict[search_name]
     except:
         print("Debug unable to find serovar in the json file")
    
     
-    h_antigen = filtered_dict.get("H-Antigen", "unknown")
-    o_antigen_p1 = filtered_dict.get("O-AntigenP1", "unknown")
-    o_antigen_p2 = filtered_dict.get("O-AntigenP2", "unknown")   
+    o_antigen = filtered_dict.get("O-Antigen", "unknown")
+    h_antigen_p1 = filtered_dict.get("H-AntigenP1", "unknown")
+    h_antigen_p2 = filtered_dict.get("H-AntigenP2", "unknown")   
     
-    full_formula = f"{h_antigen}:{o_antigen_p1}:{o_antigen_p2}"
+    full_formula = f"{o_antigen}:{h_antigen_p1}:{h_antigen_p2}"
 
     result_dict = {
-        "h_antigen":h_antigen,
-        "o_AntigenPhase1":o_antigen_p1,
-        "o_AntigenPhase2":o_antigen_p2,
+        "o_antigen":o_antigen,
+        "h_AntigenPhase1":h_antigen_p1,
+        "h_AntigenPhase2":h_antigen_p2,
         "serovar_name":name,
         "antigenic_formula":full_formula
     }
