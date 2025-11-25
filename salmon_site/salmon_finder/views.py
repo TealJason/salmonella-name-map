ab1b57825696b3ca9dff0019f20c24e1590f6176
TealJason <jasonthomasbeard@gmail.com>
2025-11-25 16:04:39 +0000

    fix to not include key in script

--- salmon_site/salmon_finder/views.py
+++ salmon_site/salmon_finder/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .utils import run_lookup_logic
 import os
+from django.conf import settings
+from django.shortcuts import render
 
 def find_serovar(request):
     if request.method == "GET":
-        return render(request, "salmon_finder/search.html")
+        return render(request, "salmon_finder/search.html", {
+            "MAPBOX_TOKEN": settings.MAPBOX_API_KEY
+        })
 
     if request.method == "POST":
         lat = request.POST.get("lat")
@@ -15,16 +19,11 @@ def find_serovar(request):
         lat = float(lat) if lat else None
         long = float(long) if long else None
 
-        result,mapbox_image = run_lookup_logic(lat, long, place_name,get_image, verbose=True)
+        result, mapbox_image = run_lookup_logic(lat, long, place_name, get_image, verbose=True)
 
-        # Pass result data to the same search template
         status = 200 if "error" not in result else 400
-        return render(request, "salmon_finder/search.html", {"result": result, "mapbox_image": mapbox_image})
-    
-from django.conf import settings
-from django.shortcuts import render
-
-def map_view(request):
-    return render(request, "map.html", {
-        "MAPBOX_TOKEN": settings.MAPBOX_API_KEY
-    })
+        return render(request, "salmon_finder/search.html", {
+            "result": result,
+            "mapbox_image": mapbox_image,
+            "MAPBOX_TOKEN": settings.MAPBOX_API_KEY   # <-- YOU MUST ADD THIS
+        })
